1f5b2e88acc2be08d050aa3ef7b8f8f1d56d71bd
Amos Lastmann <a.s.lastmann@gmail.com>
2025-08-26 23:05:19 +0200

    feat: add structure reading and readme with course tickboxes

--- structure.py
+++ structure.py
@@ -1,1 +1,15 @@
+import os
+
 OUTPUT = open("README.md", "w+")
+
+main_dir = "Part III Guide to Courses"
+sections = os.listdir(main_dir)
+
+for section in sections:
+    OUTPUT.write(f"# {section}\n\n")
+    for subsection in os.listdir(f"{main_dir}\\{section}"):
+        OUTPUT.write(f"## {subsection}\n")
+        for file in os.listdir(f"{main_dir}\\{section}\\{subsection}"):
+            OUTPUT.write(f"[ ] {file.replace(".pdf", "")}\n")
+        OUTPUT.write("\n")
+    OUTPUT.write("\n")
